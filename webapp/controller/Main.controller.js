62a6a03869cec92baefd3c934b15c4d892bddddc
Fankyzene <marjai.janos@gmail.com>
2024-03-21 14:35:08 +0100

    Fix for routing data transfer

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -8,15 +8,15 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("project1.controller.Main", {
-            onPress: function(oEvent) {
+            onPress: function (oEvent) {
                 var oItem = oEvent.getSource();
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("Service", {
-            }
-                )
-        }});
+                  servicePath: window.encodeURIComponent(
+                    oItem.getBindingContext("services").getPath().substr(1)
+                  ),
+                });
+              },
+            });
     
     });
-    
-        
-      
--- webapp/controller/Service.controller.js
+++ webapp/controller/Service.controller.js
@@ -7,7 +7,18 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("project1.controller.Main", {
+        return Controller.extend("project1.controller.Service", {
+            onInit() {
+                const oRouter = this.getOwnerComponent().getRouter();
+                oRouter.getRoute("Service").attachPatternMatched(this.onObjectMatched, this);
+            },
+    
+            onObjectMatched(oEvent) {
+                this.getView().bindElement({
+                    path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").servicePath),
+                    model: "services"
+                });
+            },
             onPress: function (oEvent) {
                 var oItem = oEvent.getSource();
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
