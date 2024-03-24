3c5f616b785634adf2bf229471e2c1602ddc0af7
Fankyzene <marjai.janos@gmail.com>
2024-03-24 14:34:47 +0100

    Fix data

--- webapp/controller/Service.controller.js
+++ webapp/controller/Service.controller.js
@@ -26,231 +26,113 @@ sap.ui.define([
 
                 var oModel = new JSONModel();
                 oModel.setData({
-                        startDate: UI5Date.getInstance("2018", "6", "9"),
+                        startDate: UI5Date.getInstance("2024", "03", "8"),
                         appointments: [{
-                            title: "Meet John Miller",
+                            title: "Tóbiás József",
                             type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "5", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "6", "0")
+                            startDate: UI5Date.getInstance("2024", "03", "8", "9", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "8", "10", "0")
                         }, {
-                            title: "Discussion of the plan",
+                            title: "Kerék Ágnes",
                             type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "6", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "7", "9")
+                            startDate: UI5Date.getInstance("2024", "03", "8", "8", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "8", "9", "9")
                         }, {
-                            title: "Lunch",
-                            text: "canteen",
+                            title: "Döbrögi Kálmán",
+                            text: "K1600GT",
                             type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "7", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "8", "0")
+                            startDate: UI5Date.getInstance("2024", "03", "8", "11", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "8", "12", "0")
                         }, {
-                            title: "New Product",
-                            text: "room 105",
+                            title: "Béla Ferenc",
+                            text: "1200GS",
                             type: CalendarDayType.Type01,
                             icon: "sap-icon://meeting-room",
-                            startDate: UI5Date.getInstance("2018", "6", "8", "8", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "9", "0")
+                            startDate: UI5Date.getInstance("2024", "03", "8", "10", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "8", "11", "0")
                         }, {
-                            title: "Team meeting",
-                            text: "Regular",
+                            title: "M1000RR Workshop",
+                            text: "Csapat event",
                             type: CalendarDayType.Type01,
                             icon: "sap-icon://home",
-                            startDate: UI5Date.getInstance("2018", "6", "8", "9", "9"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "10", "0")
+                            startDate: UI5Date.getInstance("2024", "03", "8", "9", "9"),
+                            endDate: UI5Date.getInstance("2024", "03", "8", "10", "0")
                         }, {
-                            title: "Discussion with clients regarding our new purpose",
-                            text: "room 234 and Online meeting",
+                            title: "Géza István",
+                            text: "F650GS",
                             type: CalendarDayType.Type08,
                             icon: "sap-icon://home",
-                            startDate: UI5Date.getInstance("2018", "6", "8", "10", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "11", "30")
+                            startDate: UI5Date.getInstance("2024", "03", "8", "10", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "8", "11", "30")
                         }, {
-                            title: "Discussion of the plan",
-                            text: "Online meeting with partners and colleagues",
+                            title: "Mamlasz Judit",
+                            text: "GT",
                             type: CalendarDayType.Type01,
                             icon: "sap-icon://home",
                             tentative: true,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "11", "30"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "13", "00")
+                            startDate: UI5Date.getInstance("2024", "03", "8", "11", "30"),
+                            endDate: UI5Date.getInstance("2024", "03", "8", "13", "00")
                         }, {
-                            title: "Discussion with clients",
-                            type: CalendarDayType.Type08,
-                            icon: "sap-icon://home",
-                            startDate: UI5Date.getInstance("2018", "6", "8", "12", "30"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "13", "15")
-                        }, {
-                            title: "Meeting with the manager",
+                            title: "Olajszállítás",
                             type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "13", "9"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "13", "9")
+                            startDate: UI5Date.getInstance("2024", "03", "9", "6", "30"),
+                            endDate: UI5Date.getInstance("2024", "03", "9", "7", "0")
                         }, {
-                            title: "Meeting with the HR",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "14", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "14", "15")
-                        }, {
-                            title: "Call with customer",
-                            type: CalendarDayType.Type08,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "14", "15"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "14", "30")
-                        }, {
-                            title: "Prepare documentation",
-                            text: "At my desk",
-                            icon: "sap-icon://meeting-room",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "8", "14", "10"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "15", "30")
-                        }, {
-                            title: "Meeting with the manager",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "9", "6", "30"),
-                            endDate: UI5Date.getInstance("2018", "6", "9", "7", "0")
-                        }, {
-                            title: "Lunch",
+                            title: "Fékszállítmány",
                             type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "9", "7", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "9", "8", "0")
+                            startDate: UI5Date.getInstance("2024", "03", "9", "7", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "9", "8", "0")
                         }, {
-                            title: "Team meeting",
-                            text: "online",
+                            title: "Béla Kálmán",
+                            text: "1150RT",
                             type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "9", "8", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "9", "9", "0")
-                        }, {
-                            title: "Discussion with clients for the new release dates",
-                            text: "Online meeting",
-                            type: CalendarDayType.Type08,
-                            startDate: UI5Date.getInstance("2018", "6", "9", "9", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "9", "10", "0")
-                        }, {
-                            title: "Team meeting",
-                            text: "room 5",
-                            type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "9", "11", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "9", "14", "0")
-                        }, {
-                            title: "Daily standup meeting",
-                            type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "9", "9", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "9", "9", "15", "0")
-                        }, {
-                            title: "Private meeting",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "11", "9", "9"),
-                            endDate: UI5Date.getInstance("2018", "6", "11", "9", "20")
-                        }, {
-                            title: "Private meeting",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "10", "6", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "10", "7", "0")
-                        }, {
-                            title: "Meeting with the manager",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "10", "15", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "10", "15", "30")
-                        }, {
-                            title: "Meet John Doe",
+                            startDate: UI5Date.getInstance("2024", "03", "9", "8", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "9", "9", "0")
+                        },{
+                            title: "Andrea Moretti",
                             type: CalendarDayType.Type05,
-                            icon: "sap-icon://home",
-                            startDate: UI5Date.getInstance("2018", "6", "11", "7", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "11", "7", "30")
+                            startDate: UI5Date.getInstance("2024", "03", "10", "9", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "10", "10", "0")
                         }, {
-                            title: "Team meeting",
-                            text: "online",
+                            title: "Toyo Ta",
                             type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "11", "8", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "11", "9", "30")
+                            startDate: UI5Date.getInstance("2024", "03", "10", "8", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "10", "9", "9")
                         }, {
-                            title: "Workshop",
+                            title: "Suzu Ki",
+                            text: "K1600GT",
                             type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "11", "8", "30"),
-                            endDate: UI5Date.getInstance("2018", "6", "11", "12", "0")
+                            startDate: UI5Date.getInstance("2024", "03", "10", "11", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "10", "12", "0")
                         }, {
-                            title: "Team collaboration",
-                            type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "12", "4", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "12", "12", "30")
-                        }, {
-                            title: "Out of the office",
-                            type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "12", "15", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "12", "19", "30")
-                        }, {
-                            title: "Working out of the building",
-                            type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "12", "20", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "12", "21", "30")
-                        }, {
-                            title: "Vacation",
-                            type: CalendarDayType.Type09,
-                            text: "out of office",
-                            startDate: UI5Date.getInstance("2018", "6", "11", "12", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "13", "14", "0")
-                        }, {
-                            title: "Reminder",
-                            type: CalendarDayType.Type09,
-                            startDate: UI5Date.getInstance("2018", "6", "12", "00", "00"),
-                            endDate: UI5Date.getInstance("2018", "6", "13", "00", "00")
-                        }, {
-                            title: "Team collaboration",
-                            type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "6", "00", "00"),
-                            endDate:  UI5Date.getInstance("2018", "6", "16", "00", "00")
-                        }, {
-                            title: "Workshop out of the country",
-                            type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "14", "00", "00"),
-                            endDate: UI5Date.getInstance("2018", "6", "20", "00", "00")
-                        }, {
-                            title: "Payment reminder",
-                            type: CalendarDayType.Type09,
-                            startDate: UI5Date.getInstance("2018", "6", "7", "00", "00"),
-                            endDate: UI5Date.getInstance("2018", "6", "8", "00", "00")
-                        }, {
-                            title:"Meeting with the manager",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "6", "9", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "6", "10", "0")
-                        }, {
-                            title:"Daily standup meeting",
-                            type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "7", "10", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "7", "10", "30")
-                        }, {
-                            title:"Private meeting",
-                            type: CalendarDayType.Type03,
-                            startDate: UI5Date.getInstance("2018", "6", "6", "11", "30"),
-                            endDate: UI5Date.getInstance("2018", "6", "6", "12", "0")
-                        }, {
-                            title:"Lunch",
-                            type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "6", "12", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "6", "13", "0")
-                        }, {
-                            title:"Discussion of the plan",
-                            type: CalendarDayType.Type01,
-                            startDate: UI5Date.getInstance("2018", "6", "16", "11", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "16", "12", "0")
-                        }, {
-                            title:"Lunch",
-                            text: "canteen",
-                            type: CalendarDayType.Type05,
-                            startDate: UI5Date.getInstance("2018", "6", "16", "12", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "16", "13", "0")
-                        }, {
-                            title:"Team meeting",
-                            text: "room 200",
+                            title: "Beem Ve",
+                            text: "1200GS",
                             type: CalendarDayType.Type01,
                             icon: "sap-icon://meeting-room",
-                            startDate:  UI5Date.getInstance("2018", "6", "16", "16", "0"),
-                            endDate: UI5Date.getInstance("2018", "6", "16", "17", "0")
+                            startDate: UI5Date.getInstance("2024", "03", "10", "10", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "10", "11", "0")
                         }, {
-                            title:"Discussion with clients",
-                            text: "Online meeting",
+                            title: "Kava Szaki",
+                            text: "Csapat event",
+                            type: CalendarDayType.Type01,
+                            icon: "sap-icon://home",
+                            startDate: UI5Date.getInstance("2024", "03", "10", "9", "9"),
+                            endDate: UI5Date.getInstance("2024", "03", "10", "10", "0")
+                        }, {
+                            title: "Da Kar",
+                            text: "F650GS",
                             type: CalendarDayType.Type08,
                             icon: "sap-icon://home",
-                            startDate: UI5Date.getInstance("2018", "6", "17", "15", "30"),
-                            endDate: UI5Date.getInstance("2018", "6", "17", "16", "30")
+                            startDate: UI5Date.getInstance("2024", "03", "10", "10", "0"),
+                            endDate: UI5Date.getInstance("2024", "03", "10", "11", "30")
+                        }, {
+                            title: "Assetto Corsa",
+                            text: "GT",
+                            type: CalendarDayType.Type01,
+                            icon: "sap-icon://home",
+                            tentative: true,
+                            startDate: UI5Date.getInstance("2024", "03", "10", "11", "30"),
+                            endDate: UI5Date.getInstance("2024", "03", "10", "13", "00")
                         }
                     ],
                     supportedAppointmentItems: [
